480757eb7b7c97615930498361e0c65e143bdac3
iluvator <petrov34rus@yandex.ru>
2023-10-10 17:51:24 +0300

    Повторное исправление ошибок во 2 задании

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,24 @@
-const cheakingLengthString = (string, length) => string.length <= length;
+const cheakLengthString = function (string, length) {
+  return string.length <= length;
+};
 
-const checkingPalindrome = function (line) {
+const checkPalindrome = function (line) {
   const normalizeString = line.toLowerCase().replaceAll(' ', '');
   let comparisonString = '';
   for(let i = normalizeString.length - 1; i >= 0; i--) {
-    comparisonString += normalizeString[i].at();
+    comparisonString += normalizeString[i];
   }
   return normalizeString === comparisonString;
 };
 
-const findingNumbers = function (string) {
+const findNumbers = function (string) {
   const value = string.toString();
   let result = '';
-  for(let i = 0; i < value.length; i++){
+  for (let i = 0; i < value.length; i++){
     if(!(isNaN(parseInt(value[i], 10)))){
       result += value[i];
     }
   }
   return (result !== '') ? result : NaN;
 };
-
 
