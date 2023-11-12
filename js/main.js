aba4f4802755b0936ee766f946ee76ae48e57747
iluvator <petrov34rus@yandex.ru>
2023-11-12 20:20:14 +0300

    Разбил код файла form (выполняющий добавление и редактирования фотографий) на модули

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import {createPhotos, COUNT_PICTURE} from './data.js';
 import {createElementPhotos} from './pictures.js';
-import { ZoomOption, editZoomPicture } from './edit-upload-pictures.js';
-import './form.js';
+import './uploadPictures-modal.js';
+
 
 const listDataPictures = createPhotos(COUNT_PICTURE);
 createElementPhotos(listDataPictures);
 
-editZoomPicture(ZoomOption);
+
